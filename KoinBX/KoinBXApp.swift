4577a8727047aa59d94d185710753891eba6c13d
Guru <guru@chingari.io>
2025-04-22 15:04:07 +0530

    Initial Commit

---
+++ KoinBX/KoinBXApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoinBXApp.swift
+//  KoinBX
+//
+//  Created by Guru on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct KoinBXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
